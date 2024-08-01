6688c8a72291e1975776ead54c5864f552a7f9bd
Arzel John Zolina <Arzeljrz17@gmail.com>
2024-08-01 10:42:04 +0800

    set up backend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  constructor(private productsService: ProductsService) {}
+  ngOnInit() {
+    this.productsService
+      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 10 })
+      .subscribe((products) => {
+        console.log(products);
+      });
+  }
 }

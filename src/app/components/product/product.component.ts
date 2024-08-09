d8c4cb3d7cc878e2f88a2a2c270cc62681c73a8b
Arzel John Zolina <Arzeljrz17@gmail.com>
2024-08-09 14:54:44 +0800

    done components, routing and modules

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,20 +1,72 @@
-import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  output,
+  ViewChild,
+  viewChild,
+} from '@angular/core';
 import { Product } from '../../../types';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmationService } from 'primeng/api';
+import { PricePipe } from '../../pipes/price.pipe';
+import { TruncateNamePipe } from '../../pipes/truncate-name.pipe';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [RatingModule, FormsModule],
+  imports: [
+    RatingModule,
+    FormsModule,
+    ButtonModule,
+    ConfirmPopupModule,
+    ToastModule,
+    PricePipe,
+    TruncateNamePipe,
+  ],
+
+  providers: [ConfirmationService],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
 export class ProductComponent {
+  constructor(private confirmService: ConfirmationService) {}
+  @ViewChild('deleteButton') deleteButton: any;
+
   @Input() product!: Product;
-  @Output() productOutput: EventEmitter<Product> = new EventEmitter<Product>();
+  @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
+  @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
+
+/*   truncateName(name: string) {
+    if (name.length > 16) {
+      return name.slice(0, 16) + '...';
+    }
+    return name;
+  }
+ */
+  editProduct() {
+    this.edit.emit(this.product);
+  }
+
+  confirmDelete() {
+    this.confirmService.confirm({
+      target: this.deleteButton.nativeElement,
+      message: 'Are you sure that you want to delete this product?',
+      accept: () => {
+        this.deleteProduct();
+      },
+    });
+  }
+  deleteProduct() {
+    this.delete.emit(this.product);
+  }
 
   ngOnInit() {
-    this.productOutput.emit(this.product);
+    /*  this.edit.emit(this.product); */
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,32 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
-import { PaginatorModule } from 'primeng/paginator';
+import { Paginator, PaginatorModule } from 'primeng/paginator';
 import { DialogModule } from 'primeng/dialog';
-
+import { EditPopupComponent } from '../components/edit-popup/edit-popup.component';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule, PaginatorModule, DialogModule],
+  imports: [
+    ProductComponent,
+    CommonModule,
+    PaginatorModule,
+    /*   DialogModule, */
+    EditPopupComponent,
+    ButtonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  /*   paginator: any; */
   constructor(private productsService: ProductsService) {}
+
+  @ViewChild('paginator') paginator: Paginator | undefined;
 
   products: Product[] = [];
 
   totalRecords: number = 0;
   rows: number = 5;
 
+  displayEditPopup: boolean = false;
+  displayAddPopup: boolean = false;
+
+  toggleEditPopup(product: Product) {
+    this.selectedProduct = product;
+    this.displayEditPopup = true;
+  }
+
+  toggleDeletePopup(product: Product) {
+    if (!product.id) {
+      return;
+    }
+    this.deleteProduct(product.id);
+  }
+
+  toggleAddPopup() {
+    this.displayAddPopup = true;
+  }
+
+  selectedProduct: Product = {
+    id: 0,
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  };
+
+  onConfirmEdit(product: Product) {
+    if (!this.selectedProduct.id) {
+      return;
+    }
+    this.editProduct(product, this.selectedProduct.id);
+    this.displayEditPopup = false;
+  }
+
+  onConfirmAdd(product: Product) {
+    this.addProduct(product);
+    this.displayAddPopup = false;
+  }
   onProductOutput(product: Product) {
     console.log(product, 'Output');
   }
 
   onPageChange(event: any) {
     this.fetchProducts(event.page, event.rows);
+  }
+
+  resetPaginator() {
+    this.paginator?.changePage(0);
   }
 
   fetchProducts(page: number, perPage: number) {
@@ -36,6 +90,7 @@ export class HomeComponent {
         next: (data: Products) => {
           this.products = data.items;
           this.totalRecords = data.total;
+          this.resetPaginator();
         },
         error: (error) => {
           console.log(error);
@@ -47,11 +102,12 @@ export class HomeComponent {
     /*  console.log(product, 'Edit'); */
 
     this.productsService
-      .editProduct(`https://localhost/clothes/${id}`, product)
+      .editProduct(`http://localhost:3000/clothes/${id}`, product)
       .subscribe({
         next: (data) => {
           console.log(data);
           this.fetchProducts(0, this.rows);
+          this.resetPaginator();
         },
         error: (error) => {
           console.log(error);
@@ -59,15 +115,16 @@ export class HomeComponent {
       });
   }
 
-  addProduct(product: Product, id: number) {
+  addProduct(product: Product) {
     /*  console.log(product, 'Add'); */
 
     this.productsService
-      .addProduct(`https://localhost/clothes`, product)
+      .addProduct(`http://localhost:3000/clothes`, product)
       .subscribe({
         next: (data) => {
           console.log(data);
           this.fetchProducts(0, this.rows);
+          this.resetPaginator();
         },
         error: (error) => {
           console.log(error);
@@ -78,11 +135,12 @@ export class HomeComponent {
     /*   console.log(product, 'Delete'); */
 
     this.productsService
-      .deleteProduct(`https://localhost/clothes/${id}`)
+      .deleteProduct(`http://localhost:3000/clothes/${id}`)
       .subscribe({
         next: (data) => {
           console.log(data);
           this.fetchProducts(0, this.rows);
+          /*  this.resetPaginator(); */
         },
         error: (error) => {
           console.log(error);
